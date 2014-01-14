4d5be322ce1d69fb9a28f0da3b736e75f386f65f
Nick Craig-Wood <nick@craig-wood.com>
2014-01-14 23:56:42 +0000

    Transpose matrices

---
+++ transpose.go
@@ -0,0 +1,122 @@
+// Transpose a matrix
+
+// Based on code posted on sci.math.num-analysis by Jason Stratos Papadopoulos
+//
+// https://groups.google.com/forum/?hl=en#!msg/sci.math.num-analysis/qbqu8eEbaZ0/xDCs-7MRAmgJ
+
+// The functions here are for a very fast matrix transpose, needed
+// for Bailey's four-step FFT. Some assumptions: the matrix must
+// be square, must be a power of two on a side, and must be stored
+// as a 1-D array. It uses block transposition; divide the matrix
+// into blocks of a certain optimal size; then transpose the diagonal
+// blocks and transpose/switch the off-diagonal blocks. BL below is the
+// power of two defining block size.
+//
+// There's plenty of room for assembly optimization, but right now the
+// time is dominated by cache misses, and tweaking code here and there
+// won't help too much.
+
+package main
+
+const BL = 5
+
+// Move a BLxBL block from matrix "startpt" (of size
+// 1<<startlength on a side) to matrix "destpt" (of
+// size 1<<destlength on a side).
+func copyblock(start []uint64, startpt, startlength int, dest []uint64, destpt, destlength int) {
+	for i := 0; i < (1 << BL); i++ {
+		// FIXME do we need to get dest in the cache before starting the copy?
+		copy(dest[destpt:destpt+(1<<BL)], start[startpt:startpt+(1<<BL)])
+		startpt += startlength /* next row of startpt */
+		destpt += destlength   /* next row of destpt */
+	}
+}
+
+// This function switches the columns in the BLxBL block
+// "startpt" with the rows of the BLxBL block in "destpt".
+// This also has the effect of transposing the block in
+// destpt, which need only then be copied into its appropriate
+// place. The code below pairs nicely, and performs the
+// copying while all the data involved is in cache.
+func copytranspose(start []uint64, startpt int, dest []uint64, destpt int, destlength int) {
+	for i := 0; i < (1 << BL); i++ {
+		src := startpt
+		dst := destpt
+		for j := 0; j < (1 << BL); j += 8 {
+			dest[dst], start[src] = start[src], dest[dst]
+			dest[dst+1], start[src+1<<BL] = start[src+1<<BL], dest[dst+1]
+			dest[dst+2], start[src+2<<BL] = start[src+2<<BL], dest[dst+2]
+			dest[dst+3], start[src+3<<BL] = start[src+3<<BL], dest[dst+3]
+			dest[dst+4], start[src+4<<BL] = start[src+4<<BL], dest[dst+4]
+			dest[dst+5], start[src+5<<BL] = start[src+5<<BL], dest[dst+5]
+			dest[dst+6], start[src+6<<BL] = start[src+6<<BL], dest[dst+6]
+			dest[dst+7], start[src+7<<BL] = start[src+7<<BL], dest[dst+7]
+			src += 8 << BL
+			dst += 8
+		}
+		startpt += 1         /* move to next column */
+		destpt += destlength /* move to next row */
+	}
+}
+
+// Transpose an array "x". "scratch" must be the size
+// of one full block (BL*BL), and "size" is the power of two x
+// is on a side (e.g. 512x512 matrix has side=9). The loop
+// below transposes block (1,1) and then moves down the first
+// row and first column of blocks, switching their transposes.
+// Then the process repeats for the second row and column, etc
+func TransposeSquareFast(x, scratch []uint64, log_side uint8) []uint64 {
+	if log_side < BL {
+		panic("Too small to transpose")
+	}
+	if len(scratch) < (1 << (2 * BL)) {
+		panic("Scratch space too small")
+	}
+	corner := 0
+	blocks := log_side - BL
+	for col := 1; col <= 1<<blocks; col++ {
+		copyblock(x, corner, 1<<log_side, scratch, 0, 1<<BL) /* diagonals */
+		copytranspose(scratch, 0, x, corner, 1<<log_side)
+		for row := 1; row < (1<<blocks)-col+1; row++ {
+			copyblock(x, corner+(row<<BL), 1<<log_side, scratch, 0, 1<<BL) /* off-diagonals */
+			copytranspose(scratch, 0, x, corner+(row<<(log_side+BL)), 1<<log_side)
+			copyblock(scratch, 0, 1<<BL, x, corner+(row<<BL), 1<<log_side)
+		}
+		/* move down and to the right */
+		corner += (1 << (log_side + BL)) + (1 << BL)
+	}
+	return x
+}
+
+// Transpose a square matrix
+func TransposeSquareSlow(x []uint64, log_side uint8) []uint64 {
+	side := 1 << log_side
+	for i := 0; i < side; i++ {
+		for j := 0; j < side; j++ {
+			a := (j << log_side) + i
+			b := (i << log_side) + j
+			if a < b {
+				x[a], x[b] = x[b], x[a]
+			}
+		}
+	}
+	return x
+}
+
+// Transpose a matrix
+//
+// Pass in some scratch space which should be the same size as x
+//
+// It returns x or the scratch space whichever has the answer in
+func Transpose(x, scratch []uint64, log_cols uint8, log_rows uint8) []uint64 {
+	if log_cols == log_rows {
+		if log_cols < BL {
+			return TransposeSquareSlow(x, log_cols)
+		} else {
+			return TransposeSquareFast(x, scratch, log_cols)
+		}
+	} else {
+		panic("Can't only inplace transpose square matrices at the moment")
+	}
+	return nil
+}
---
+++ transpose_test.go
@@ -0,0 +1,88 @@
+// Test the transpose routines
+
+package main
+
+import (
+	"testing"
+)
+
+func TestTransposeSlow2(t *testing.T) {
+	x := []uint64{
+		1, 2, 3, 4,
+		5, 6, 7, 8,
+		9, 10, 11, 12,
+		13, 14, 15, 16,
+	}
+	y := []uint64{
+		1, 5, 9, 13,
+		2, 6, 10, 14,
+		3, 7, 11, 15,
+		4, 8, 12, 16,
+	}
+	TransposeSquareSlow(x, 2)
+	for i := range x {
+		if x[i] != y[i] {
+			t.Fatal("Failed %d: expecting %d got %d\n", i, x[i], y[i])
+		}
+	}
+}
+
+func testTransposeFastN(t *testing.T, log_side uint8) {
+	n := uint(1) << (2 * log_side)
+	x := make([]uint64, n)
+	scratch := make([]uint64, n)
+	for i := range x {
+		x[i] = uint64(i)
+	}
+	TransposeSquareFast(x, scratch, log_side)
+	if x[0] != 0 || x[1] != 1<<log_side || x[1<<log_side] != 1 {
+		t.Fatalf("Transpose failed x[0]=%d, x[1]=%d, x[%d]=%d", x[0], x[1], 1<<log_side, x[1<<log_side])
+	}
+	TransposeSquareSlow(x, log_side)
+	for i := range x {
+		if x[i] != uint64(i) {
+			t.Fatalf("Failed x[%d]: expecting %d got %d\n", i, i, x[i])
+		}
+	}
+}
+
+func TestTransposeFast5(t *testing.T) {
+	testTransposeFastN(t, 5)
+}
+
+func TestTransposeFast6(t *testing.T) {
+	testTransposeFastN(t, 6)
+}
+
+func TestTransposeFast7(t *testing.T) {
+	testTransposeFastN(t, 7)
+}
+
+func TestTransposeFast8(t *testing.T) {
+	testTransposeFastN(t, 8)
+}
+
+func TestTransposeFast9(t *testing.T) {
+	testTransposeFastN(t, 9)
+}
+
+func BenchmarkTransposeSlow10(b *testing.B) {
+	b.StopTimer()
+	n := uint(1) << (2 * 10)
+	x := make([]uint64, n)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		TransposeSquareSlow(x, 10)
+	}
+}
+
+func BenchmarkTransposeFast10(b *testing.B) {
+	b.StopTimer()
+	n := uint(1) << (2 * 10)
+	x := make([]uint64, n)
+	scratch := make([]uint64, n)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		TransposeSquareFast(x, scratch, 10)
+	}
+}
